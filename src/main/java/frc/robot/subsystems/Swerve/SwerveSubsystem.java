8451014079c46d8c94e0b634de8fd7d1b3dd75f7
Landon <landon.fehrenbacher@evsck12.com>
2025-02-15 14:08:59 -0600

    Enhance SwerveSubsystem: add simulation flag and post chassis speeds to SmartDashboard

--- src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
@@ -99,6 +99,7 @@ public class SwerveSubsystem extends SubsystemBase
 
     // Configure the Telemetry before creating the SwerveDrive to avoid unnecessary objects being created.
     SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+    SwerveDriveTelemetry.isSimulation = false;
     try
     {
        swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
@@ -138,6 +139,14 @@ public class SwerveSubsystem extends SubsystemBase
     // Update odometry with vision estimates from both cameras
     updateAllVisionEstimates();
     swerveDrive.updateOdometry();
+    SmartDashboard.putNumber("Robot/X", getPose().getX());
+    SmartDashboard.putNumber("Robot/Y", getPose().getY());
+    SmartDashboard.putNumber("Robot/Heading", getHeading().getDegrees());
+  // Post chassis speeds to SmartDashboard
+  ChassisSpeeds speeds = getRobotVelocity();
+  SmartDashboard.putNumber("Robot/VX", speeds.vxMetersPerSecond);
+  SmartDashboard.putNumber("Robot/VY", speeds.vyMetersPerSecond);
+  SmartDashboard.putNumber("Robot/Omega", speeds.omegaRadiansPerSecond);
   }
    /**
    * Construct the swerve drive.
