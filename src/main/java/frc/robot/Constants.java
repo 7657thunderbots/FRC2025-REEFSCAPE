950191aa6c513a7d8072cdf2b78a9a5c4b9d5e60
Landon <landon.fehrenbacher@evsck12.com>
2025-02-16 18:31:20 -0600

    Update dependencies and configurations: upgrade GradleRIO version, modify SwerveSubsystem parameters, and adjust AprilTag field layout; remove obsolete vendor files.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -52,7 +52,7 @@ public final class Constants {
 
       // The layout of the AprilTags on the field
       public static final AprilTagFieldLayout kTagLayout =
-              AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+              AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
 
       // The standard deviations of our vision estimated poses, which affect correction rate
       // (Fake values. Experiment and determine estimation noise on an actual robot.)
