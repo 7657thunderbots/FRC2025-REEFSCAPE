56fb950afd9985a1e89bf76e4320cefb5ecff460
LeopardDrager <maguirewmosby@gmail.com>
2025-03-31 12:14:03 -0500

    Added color presets.

--- src/main/java/frc/robot/subsystems/led/LED.java
+++ src/main/java/frc/robot/subsystems/led/LED.java
@@ -4,27 +4,19 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class LED extends SubsystemBase { // using subsystem to keep robot function clean af
+public class led extends SubsystemBase { // using subsystem to keep robot function clean af
     private final AddressableLED m_led;
     private final AddressableLEDBuffer m_ledBuffer;
 
-    public LED() {
-        m_led = new AddressableLED(3);
-        m_ledBuffer = new AddressableLEDBuffer(60);
+    public led(int port, int length) {
+        m_led = new AddressableLED(port);
+        m_ledBuffer = new AddressableLEDBuffer(length);
         m_led.setLength(m_ledBuffer.getLength());
         m_led.setData(m_ledBuffer);
         m_led.start();
     }
 
     // I used chatgpt for this part below as I have no idea how to convert it
-    public void setLEDColorHSV(int index, double h, double s, double v) {
-        int rgb = hsvToRgb(h, s, v);
-        int r = (rgb >> 16) & 0xFF;
-        int g = (rgb >> 8) & 0xFF;
-        int b = rgb & 0xFF;
-        m_ledBuffer.setRGB(index, r, g, b);
-        m_led.setData(m_ledBuffer);
-    }
 
     public void clearLEDs() {
         for (int i = 0; i < m_ledBuffer.getLength(); i++) {
@@ -33,8 +25,8 @@ public class LED extends SubsystemBase { // using subsystem to keep robot functi
         m_led.setData(m_ledBuffer);
     }
 
-    // I used chatgpt for this part below as I know with hsv you get better over
-    // brightness
+    // I used chatgpt for this part below as I know with hsv you get better control
+    // over brightness
     private int hsvToRgb(double h, double s, double v) {
         int r, g, b;
 
@@ -90,7 +82,28 @@ public class LED extends SubsystemBase { // using subsystem to keep robot functi
         int b = rgb & 0xFF;
         for (int i = 0; i < m_ledBuffer.getLength(); i++) {
             m_ledBuffer.setRGB(i, r, g, b);
+            m_led.setData(m_ledBuffer);
         }
-        m_led.setData(m_ledBuffer);
+
+    }
+
+    public void setGreen() {// we do see a tag
+        setAllLEDsColorHSV((117 / 360), 1, 1);
+    }
+
+    public void setRed() {// we don't see a tag
+        setAllLEDsColorHSV((348 / 360), 1, 1);
+    }
+
+    public void setOrange() {// we are autodriving
+        setAllLEDsColorHSV((27 / 360), (98 / 100), (95 / 100)); // dividing by 360 because hue is a range of 0-360 and
+                                                                // we are doing it in terms of 0-1.
+        // Then dividing by 100 because saturation and value are in range from 0-100,
+        // but we are using 0-1
+    }
+
+    public void blinkGreen() {
+        setAllLEDsColorHSV((117 / 360), 1, 1);
+
     }
 }
